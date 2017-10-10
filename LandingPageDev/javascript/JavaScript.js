1a8731935dcea7c5d683b3cded71904db2f117b8
Carson Samoian <carsonsamoian@gmail.com>
2017-10-10 13:09:24 -0700

    Search Letters for Items and Locations
    Searching by letter for items and locations works now. Mostly changed javascript file. Some small changes to index.html file.

--- LandingPageDev/javascript/JavaScript.js
+++ LandingPageDev/javascript/JavaScript.js
@@ -1,21 +1,330 @@
 $(document).ready(function(){
 	var currentResultArray;
 
-	if($("#category").val() === 'items') {
-		$(".letter").click(function() {
-      var itemGet = "ajax/itemlettersearch.php?key=" + $(this).text();
-			$.get(itemGet, function(response) {
-				populateList(response);
-			});
+	// When user clickes "Search Items" button, changes #category value
+	// Default value
+	$('#searchItems').click(function() {
+		$('#category').val('items');
+		var stuff = $('#category').val();
+	});
+
+	// When user clicks "Search locations" button, changes #category value
+	$('#searchLocations').click(function() {
+		$('#category').val('locations');
+	});
+
+		$(".letterA").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=A", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=A", function(response) {
+					populateList(response);
+				});
+			}
 		});
-	} else if ($("#category").val === 'locations') {
-    var locationGet = "ajax/locationlettersearch.php?key=" + $(this).text();
-		$(".letter").click(function() {
-			$.get(locationGet, function(response) {
-				populateList(response);
-			});
+
+		$(".letterB").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=B", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=B", function(response) {
+					populateList(response);
+				});
+			}
 		});
-	}
+
+		$(".letterC").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=C", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=C", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterD").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=D", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=D", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterE").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=E", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=E", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterF").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=F", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=F", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterG").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=G", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=G", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterH").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=H", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=H", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterI").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=I", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=I", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterJ").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=J", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=J", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterK").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=K", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=K", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterL").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=L", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=L", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterM").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=M", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=M", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterN").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=N", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=N", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterO").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=O", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=O", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterP").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=P", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=P", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterQ").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=Q", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=Q", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterR").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=R", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=R", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterS").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=S", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=S", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterT").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=T", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=T", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterU").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=U", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=U", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterV").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=V", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=V", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterW").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=W", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=W", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterX").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=X", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=X", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterY").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=Y", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=Y", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
+		$(".letterZ").click(function() {
+			if($('#category').val() == 'locations') {
+				$.get("ajax/locationlettersearch.php?key=Z", function(response) {
+					populateList(response);
+				});
+			} else {
+				$.get("ajax/itemlettersearch.php?key=Z", function(response) {
+					populateList(response);
+				});
+			}
+		});
+
 	$("#itemTableBody").on('click', 'tr.itemRow', function() {
 		var choice = $("#category").val();
 
