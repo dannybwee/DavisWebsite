279ff4b66768d602f6d21c0eebbb2bcb46af6832
ryman282 <ryman282@gmail.com>
2017-10-16 20:38:51 -0700

    Add files via upload

---
+++ LandingPageDev/ajax/insert_item_no_picture.php
@@ -0,0 +1,28 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "dpw_recyclopedia");
+// Check connection
+if ($conn->connect_error) {
+    die("connection to database failed: " . $conn->connect_error);
+} 
+
+//if submit button is pressed
+if(isset($_POST['submit'])){
+
+//get all item info
+$item_name = $_POST['item_name']; 
+$item_info = $_POST['item_info'];
+$item_notes = $_POST['item_notes'];
+
+//insert into database
+$sql = "INSERT INTO items (Name, General_Info, Notes, Image_Name)
+VALUES ('$item_name', '$item_info', '$item_notes','')";
+}
+
+if ($conn->query($sql) === TRUE) {
+    echo "Item successfully added";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
