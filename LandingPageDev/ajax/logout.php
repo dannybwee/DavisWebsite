1744c8785de92dba092741c355edf2f9cc56a0e5
Jon Shamber <shamthemaster@gmail.com>
2017-10-30 00:41:45 -0700

    Getting "website" back into hardcode paths
    Yep

--- LandingPageDev/ajax/logout.php
+++ LandingPageDev/ajax/logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_destroy();
 session_unset();
-header("Location: http://localhost/LandingPageDev/index.php");
+header("Location: http://localhost/website/LandingPageDev/index.php");
 exit();
 ?>
