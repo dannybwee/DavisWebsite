27a4875002592a91acf0be95828f95ac7565630b
dannybwee <dannybwee@hotmail.com>
2017-10-31 10:21:28 -0700

    Login Button Fix
    Here

--- LandingPageDev/ajax/loginButton.php
+++ LandingPageDev/ajax/loginButton.php
@@ -1,36 +1,16 @@
 <?php
-<<<<<<< HEAD
 if (isset($_SESSION['Id'])){
-       echo
-                               "<form method = 'POST' action='./ajax/logout.php'>
-                                   <button type='submit' class='btn btn-default btn-sm pull-right'>
- 	           Logout
-              </button>
-                               </form>
-<script>var mysessionvar = 1;</script>";
-;
-
-                           } else {
-                               echo "<button type='button' class='btn btn-default btn-sm pull-right' data-toggle='modal' data-target='#loginModal'>
- 	Login
-</button>
-<script>var mysessionvar = 0;</script>"
-;
-                           }
-                       ?>
-=======
-    if (isset($_SESSION['Id'])){
     echo
         "<form method = 'POST' action='./ajax/logout.php'>
-            <button type='submit' class='btn btn-default btn-sm'>
-                Logout
-           </button>
+        <button type='submit' class='btn btn-default btn-sm pull-right'>Logout
+        </button>
         </form>
         <script>var mysessionvar = 1;</script>";
+    ;
+
     } else {
-        echo "<button type='button' id='loginButton' class='btn btn-default btn-sm' data-toggle='modal' data-target='#loginModal'>
+    echo "<button type='button' class='btn btn-default btn-sm pull-right' data-toggle='modal' data-target='#loginModal'>Login
+    </button>
     <script>var mysessionvar = 0;</script>";
-            
     }
 ?>
->>>>>>> d424daf00c3650762839b4dcc78170a1f0d057d4
