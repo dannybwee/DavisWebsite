3c773e2f372c28b493e899a3718944ab39b1838c
vien1994 <vien1994@yahoo.com>
2017-11-09 14:34:09 -0800

    Login Button invisible on center bottom

--- LandingPageDev/ajax/loginButton.php
+++ LandingPageDev/ajax/loginButton.php
@@ -2,13 +2,13 @@
 if (isset($_SESSION['Id'])){
   echo
     "<form method = 'POST' action='./ajax/logout.php'>
-    <button type='submit' class='btn btn-default btn-sm pull-right'>
+    <button type='submit' class='btn btn-default btn-sm'>
  	  Logout
     </button>
     </form>
     <script>var mysessionvar = 1;</script>";;
 } else {
-  echo "<button type='button' class='btn btn-default btn-sm pull-right' data-toggle='modal' data-target='#loginModal'>
+  echo "<button type='button' id='loginButton' class='btn btn-default btn-sm' data-toggle='modal' data-target='#loginModal'>
    	Login
     </button>
     <script>var mysessionvar = 0;</script>";
