3d008046c199f6284009601435f889065e6ae81b
vien1994 <vien1994@yahoo.com>
2017-11-09 10:00:05 -0800

    Bug fix

--- LandingPageDev/ajax/addNotification.php
+++ LandingPageDev/ajax/addNotification.php
@@ -1,10 +1,7 @@
 <?php
-
-<?php
 if (isset($_SESSION['Id'])){
 echo '
-<div class="col-xs-6 col-sm-3 hidden-xs sidenav" id="sidebar">
   <button class="btn-xs btn-primary" id="editSidebar" data-toggle = "modal" data-target= "#editSidebarModal">Add</button>
-  <ul id="sidebarBullets">;'
+  ';
 }
 ?>
